2b83633e7d1940d240490170f018136e7a52ab4b
OllieBoyne <ollieboyne@gmail.com>
2023-05-04 15:39:38 +0100

    Added requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import setup, Extension
 import numpy
 
 setup(
-    ext_modules = cythonize("mesh_labeller/rasterize_tris.pyx"),
-    include_dirs=[numpy.get_include()]
+    ext_modules = Extension("mesh_labeller/rasterize_tris.pyx"),
+    include_dirs=[numpy.get_include()],
+    setup_requires=["cython"],
+    install_requires=["imageio", "numpy", "pyrender", "PyYAML", "trimesh", "pyembree", "opencv-python"]
 )
